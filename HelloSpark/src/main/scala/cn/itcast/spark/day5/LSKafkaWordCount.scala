de8f046b16c55eea86fed63bd20916c32580362f
liushuai <liushuai@shhxzq.com>
2017-08-09 15:54:17 +0800

    模拟生产逻辑处理-4-取insert且为Y的数据

--- HelloSpark/src/main/scala/cn/itcast/spark/day5/LSKafkaWordCount.scala
+++ HelloSpark/src/main/scala/cn/itcast/spark/day5/LSKafkaWordCount.scala
@@ -1,5 +1,6 @@
 package cn.itcast.spark.day5
 
+import cn.itcast.spark.day5.KafkaWordCount.updateFunc
 import com.alibaba.fastjson.JSON
 import net.sf.json.JSONObject
 import org.apache.spark.api.java.JavaPairRDD
@@ -18,6 +19,11 @@ object LSKafkaWordCount {
   //  }
   case class PlaneInfo(offset: String)
 
+  val updateFunc = (iter: Iterator[(String, Seq[Int], Option[Int])]) => {
+    //iter.flatMap(it=>Some(it._2.sum + it._3.getOrElse(0)).map(x=>(it._1,x)))
+    iter.flatMap { case (x, y, z) => Some(y.sum + z.getOrElse(0)).map(i => (x, i)) }
+  }
+
   def main(args: Array[String]) {
     LoggerLevels.setStreamingLogLevels()
     val Array(zkQuorum, group, topics, numThreads) = args
@@ -33,22 +39,27 @@ object LSKafkaWordCount {
       val data = JSON.parseObject(line._2)
       Some(data)
     })
-    val lines_insert = lines.filter(x => x.getJSONObject("header").getString("eventType").equals("INSERT"))
+    //    过滤数据取只是inset的json数据
+    val lineIn = lines.filter(x => x.getJSONObject("header").getString("eventType").equals("INSERT"))
+    //    过滤数据 取只是Y的json数据
+    val rowList = lineIn.map(x => x.getJSONArray("rowList"))
+    val yRowList = rowList.filter(x => x.getJSONObject(0).getJSONArray("afterColumns").getJSONObject(2).getString("value").equals("Y"))
+    val obj = yRowList.map(x => x.getJSONObject(0))
+    val after = obj.map(x => x.getJSONArray("afterColumns"))
+    val after1 = after.map(x => x.getJSONObject(1))
+    val valAfter1 = after1.map(x => x.getString("value"))
 
-    //    val uselines = lines.filter(_.getString())
 
-    //      取json里面表头的字段
-    val header = lines.map(x => x.getJSONObject("header"))
-    val eventType = header.map(x => x.getString("eventType"))
-//    eventType.print()
+    val data = KafkaUtils.createStream(ssc, zkQuorum, group, topicMap, StorageLevel.MEMORY_AND_DISK_SER)
+    val words = data.map(_._2).flatMap(_.split(" "))
+    val wordCounts = words.map((_, 1)).updateStateByKey(updateFunc, new HashPartitioner(ssc.sparkContext.defaultParallelism), true)
 
 
-    //    取json里面表内容字段
-    val rowList = lines.map(x => x.getJSONArray("rowList"))
 
-    //    rowList.print()
-    //      val obj = rowList.map(x => x.getJSONObject(0))
-
+    //    val obj = rowList.map(x => x.getJSONObject(0))
+    //    val afterColumns = obj.map(_.getJSONArray("afterColumns"))
+    //    val after2 = afterColumns.map(_.getJSONObject(2))
+    //    val name_af2 = after2.map(_.getString("value"))
 
     //    取json里面表内容字段
     //    val rowList = lines.map(x => x.getJSONArray("rowList"))
